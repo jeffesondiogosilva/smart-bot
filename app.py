742597f0dae04d4bd42e3271f157dfd601b0a51c
Diogo Guedes <diogoworkss@gmail.com>
2023-01-03 22:03:28 -0300

    iniciando projeto

---
+++ app.py
@@ -0,0 +1,13 @@
+import pywhatkit 
+import keyboard
+import time
+from datetime import datetime
+
+
+contatos = ['+5583999970040', '+5583981606774']
+
+while len(contatos) >= 1:
+    pywhatkit.sendwhatmsg(contatos[0],'Olá, tudo bem? Eu sou um robô', datetime.now().hour, datetime.now().minute + 1)
+    del contatos[0]
+    time.sleep(30)
+    keyboard.press_and_release('ctrl + w')
